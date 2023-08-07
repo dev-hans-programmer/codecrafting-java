d08b108fee0b2a786c795a20ca12682f0351c56b
Hasan Ali <hans1998ali@gmail.com>
2023-08-07 19:46:53 +0530

    Added dummy app to showcase how to create jar from command line

---
+++ DemoExecutable/src/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int result = Calculator.add(10, 12);
+        System.out.println("Addition result: " + result);
+    }
+}
---
+++ java-learning-project/BasicLearning/src/loops/ForEach.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class ForEach {
+    public static void useForEachLoop() {
+        String[] names = { "John", "Smith", "William" };
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
